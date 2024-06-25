5f1f577eb0e0aee5039be019c6a84b1db1a57dfd
Facundo Giuliano <facugiuliano10@gmail.com>
2024-06-25 19:23:59 -0300

    exercise 08_calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let suma = 0;
+	for (let i = 0; i < array.length; i++) {
+    suma += array[i];
+  }
+  return suma;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multi = 1;
+	for (let i = 0; i < array.length; i++) {
+    multi *= array[i];
+  }
+  return multi;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
